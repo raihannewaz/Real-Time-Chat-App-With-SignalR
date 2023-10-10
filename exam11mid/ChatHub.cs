5b549d89299b44534590c2ec970b761c53b0f197
raihannewaz <raihan_rn02@yahoo.com>
2023-10-10 22:47:35 +0600

    first commit

---
+++ exam11mid/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace exam11mid
+{
+    public class ChatHub : Hub
+    {
+        public void MsgSend(string name, string message)
+        {
+            Clients.All.BroadcastMessage(name, message);
+        }
+
+        public void FileSend(string name, string fileName, string fileData)
+        {
+            Clients.All.BroadcastFile(name, fileName, fileData);
+        }
+
+
+    }
+}
---
+++ exam11mid/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(exam11mid.Startup))]
+
+namespace exam11mid
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            app.MapSignalR();
+        }
+    }
+}
